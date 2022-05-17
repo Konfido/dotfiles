480f92781d41eb997f47af8ee71d3f4f0cc07695
Konfido <konfido.du@outlook.com>
2022-05-17 19:20:32 +0800

    add bootstrap

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-case "$1" in 
+case "$1" in
+    *.7z) 7z l "$1";;
+    *.md) glow -s dark "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *.rar) unrar l "$1";;
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
-    *.rar) unrar l "$1";;
-    *.7z) 7z l "$1";;
-    *.pdf) pdftotext "$1" -;;
-    *.md) glow -s dark "$1";;
-    #*.json) jless "$1";;
-    *) highlight -O ansi "$1" || true;;
+    *) highlight -O ansi "$1" --force || true;;
 esac
